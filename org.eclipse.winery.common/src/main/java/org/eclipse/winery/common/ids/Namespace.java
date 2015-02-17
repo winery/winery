05ba858f852e7fff4290adfe9dc2fdf9d56b2156
Oliver Kopp <kopp.dev@gmail.com>
2015-02-17 16:56:30 +0100

    Disarmed check at Namespace constructor
    Somewhere in Winery, empty Namespaces might be generated.
    Winery should be more tolerant or reject these.

--- org.eclipse.winery.common/src/main/java/org/eclipse/winery/common/ids/Namespace.java
+++ org.eclipse.winery.common/src/main/java/org/eclipse/winery/common/ids/Namespace.java
@@ -13,13 +13,19 @@ package org.eclipse.winery.common.ids;
 
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.winery.common.StringEncodedAndDecoded;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Namespace extends StringEncodedAndDecoded {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Namespace.class);
+	
 	
 	public Namespace(String uri, boolean URLencoded) {
 		super(uri, URLencoded);
 		if (StringUtils.isEmpty(uri)) {
-			throw new IllegalArgumentException("uri must not be empty or null.");
+			Namespace.logger.error("Empty URI has been passed to Namespace constructor.");
+			// throw new IllegalArgumentException("uri must not be empty or null.");
 		}
 	}
 	
