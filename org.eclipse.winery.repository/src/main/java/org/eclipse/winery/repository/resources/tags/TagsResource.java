c69d19da27e4f3173ed12f85dd40906b0d08e5d6
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2016-10-21 16:33:00 +0200

    fixes issues with tags not being added on the first try
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>

--- org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/resources/tags/TagsResource.java
+++ org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/resources/tags/TagsResource.java
@@ -25,7 +25,6 @@ import org.eclipse.winery.model.tosca.TTag;
 import org.eclipse.winery.model.tosca.TTags;
 import org.eclipse.winery.repository.resources.EntityTypeResource;
 import org.eclipse.winery.repository.resources._support.IPersistable;
-import org.eclipse.winery.repository.resources._support.collections.CollectionsHelper;
 import org.eclipse.winery.repository.resources._support.collections.withoutid.EntityWithoutIdCollectionResource;
 import org.eclipse.winery.repository.resources.entitytypeimplementations.nodetypeimplementations.NodeTypeImplementationResource;
 import org.eclipse.winery.repository.resources.entitytypeimplementations.relationshiptypeimplementations.RelationshipTypeImplementationResource;
@@ -35,90 +34,89 @@ import org.slf4j.LoggerFactory;
 
 import com.sun.jersey.api.view.Viewable;
 
-public class TagsResource extends EntityWithoutIdCollectionResource<TagResource, TTag>  {
-	
+public class TagsResource extends EntityWithoutIdCollectionResource<TagResource, TTag> {
+
 	private static final Logger logger = LoggerFactory.getLogger(TagsResource.class);
-	
+
 	public TagsResource(IPersistable res, List<TTag> list) {
-		super(TagResource.class,TTag.class,list, res);
+		super(TagResource.class, TTag.class, list, res);
 	}
-	
-	
+
 	public Viewable getHTML() {
 		return new Viewable("/jsp/tags/tags.jsp", this);
 	}
-	
+
 	/**
 	 * Adds an element using form-encoding
 	 * 
 	 * This is necessary as TRequirementRef contains an IDREF and the XML
 	 * snippet itself does not contain the target id
 	 * 
-	 * @param name the optional name of the requirement
-	 * @param value the reference to a requirement in the topology
+	 * @param name
+	 *            the optional name of the requirement
+	 * @param value
+	 *            the reference to a requirement in the topology
 	 */
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response addNewElement(@FormParam("id") String id, @FormParam("name") String name, @FormParam("value") String value) {
-		// Implementation adapted from super addNewElement
+	public Response addNewElement(@FormParam("id") String id, @FormParam("name") String name,
+			@FormParam("value") String value) {
+
+		if (!this.setTagsIfNull(name, value)) {
+			TTag tag = new TTag();
+			
+			tag.setName(name);
+			tag.setValue(value);
+			
+			return this.addNewElement(tag);
+		} else {
+			return Response.ok().build();
+		}
+
+	}
+
+	private boolean setTagsIfNull(String name, String value) {
+		TTags tags = new TTags();
 		TTag tag = new TTag();
 		
 		tag.setName(name);
 		tag.setValue(value);
 		
-		this.addTagToResource(tag);
-		
-		this.list.add(tag);
-		return CollectionsHelper.persist(this.res, this, tag);
-	}
-	
-	private void setTagsIfNull(){
-		TTags tags = null;
-		if(this.res instanceof ServiceTemplateResource){
-			tags = 	((ServiceTemplateResource)this.res).getServiceTemplate().getTags();
-			if(tags == null){
-				((ServiceTemplateResource)this.res).getServiceTemplate().setTags(new TTags());
-			}
-		} else if(this.res instanceof EntityTypeResource){
-			tags = ((EntityTypeResource)this.res).getEntityType().getTags();
-			if(tags == null){
-				((EntityTypeResource)this.res).getEntityType().setTags(new TTags());
-			}
-		} else if(this.res instanceof NodeTypeImplementationResource){
-			tags = ((NodeTypeImplementationResource)this.res).getNTI().getTags();
-			if(tags == null){
-				((NodeTypeImplementationResource)this.res).getNTI().setTags(new TTags());
-			}
-		} else if(this.res instanceof RelationshipTypeImplementationResource){
-			tags = ((RelationshipTypeImplementationResource)this.res).getRTI().getTags();
-			if(tags == null){
-				((RelationshipTypeImplementationResource)this.res).getRTI().setTags(new TTags());
-			}
-		}
-		try {
-			this.res.persist();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private void addTagToResource(TTag tag){
-		
-		this.setTagsIfNull();
-		
-		TTags tags = null;
-		if(this.res instanceof ServiceTemplateResource){
-			tags = 	((ServiceTemplateResource)this.res).getServiceTemplate().getTags();
-		} else if(this.res instanceof EntityTypeResource){
-			tags = ((EntityTypeResource)this.res).getEntityType().getTags();
-		} else if(this.res instanceof NodeTypeImplementationResource){
-			tags = ((NodeTypeImplementationResource)this.res).getNTI().getTags();
-		} else if(this.res instanceof RelationshipTypeImplementationResource){
-			tags = ((RelationshipTypeImplementationResource)this.res).getRTI().getTags();
-		}
-		
 		tags.getTag().add(tag);
+		
+		boolean createdTags = false;
+		
+		if (this.res instanceof ServiceTemplateResource) {
+			if (((ServiceTemplateResource) this.res).getServiceTemplate().getTags() == null) {
+				((ServiceTemplateResource) this.res).getServiceTemplate().setTags(tags);
+				createdTags = true;
+			}
+		} else if (this.res instanceof EntityTypeResource) {			
+			if (((EntityTypeResource) this.res).getEntityType().getTags() == null) {
+				((EntityTypeResource) this.res).getEntityType().setTags(tags);
+				createdTags = true;
+			}
+		} else if (this.res instanceof NodeTypeImplementationResource) {
+			if (((NodeTypeImplementationResource) this.res).getNTI().getTags() == null) {
+				((NodeTypeImplementationResource) this.res).getNTI().setTags(tags);
+				createdTags = true;
+			}
+		} else if (this.res instanceof RelationshipTypeImplementationResource) {
+			if (((RelationshipTypeImplementationResource) this.res).getRTI().getTags() == null) {
+				((RelationshipTypeImplementationResource) this.res).getRTI().setTags(tags);
+				createdTags = true;
+			}
+		}
+		if (createdTags) {
+			try {
+				this.res.persist();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return createdTags;
 	}
-	
+
 }
