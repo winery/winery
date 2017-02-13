737d652008a9626e652a4b80c1bbe3bf3b7d9734
Tino Stadelmaier <tstadelmaier.github@gmail.com>
2017-02-13 16:19:32 +0100

    initial commit with very basic functionality
    Signed-off-by: Tino Stadelmaier <tstadelmaier.github@gmail.com>

--- org.eclipse.winery.ui/src/app/instance/editXML/editXML.component.ts
+++ org.eclipse.winery.ui/src/app/instance/editXML/editXML.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'winery-instance-editXML',
     templateUrl: 'editXML.component.html'
 })
 export class EditXMLComponent implements OnInit {
+     xmlData: string ;
+     testdata: string;
+    @Input() areaid: string;
     constructor() {
     }
 
     ngOnInit() {
+        this.xmlData = '<?xml version="1.0" encoding="UTF-8"?><tosca:Definitions xmlns:tosca="http://docs.oasis-open.org/tosca/ns/2011/12" id="winery-devs-for_xmlns-TestName2" targetNamespace="http://www.example.org/namespace0"><tosca:NodeType name="TestName2" targetNamespace="http://www.example.org/namespace0"/></tosca:Definitions>';
+
+        this.testdata = 'xmltest';
     }
 }
