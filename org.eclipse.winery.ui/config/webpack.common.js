5a94dfe0b5cc6d10e97735a4b4d08096845d328a
Lukas Harzenetter <lharzenetter@gmx.de>
2017-03-16 17:06:07 +0100

    webpack 2
    Signed-off-by: Lukas Harzenetter <lharzenetter@gmx.de>

--- org.eclipse.winery.ui/config/webpack.common.js
+++ org.eclipse.winery.ui/config/webpack.common.js
@@ -1,25 +1,12 @@
-/**
- * Copyright (c) 2017 University of Stuttgart.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and the Apache License 2.0 which both accompany this distribution,
- * and are available at http://www.eclipse.org/legal/epl-v10.html
- * and http://www.apache.org/licenses/LICENSE-2.0
- *
- * Contributors:
- *     Lukas Harzenetter - initial API and implementation
- */
 const webpack = require('webpack');
-
-const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
-
 const helpers = require('./helpers');
+
+const path = require('path');
 
 module.exports = {
 
@@ -42,69 +29,45 @@ module.exports = {
         chunkFilename: '[id].chunk.js',
     },
 
-    externals: [
-       require('webpack-require-http')
-    ],
-
     /*
      * http://webpack.github.io/docs/configuration.html#resolve
      */
     resolve: {
         // http://webpack.github.io/docs/configuration.html#resolve-extensions
-        extensions: ['', '.ts', '.js', '.json', '.css', '.scss', '.html'],
-        // Make sure root is src
-        root: helpers.root('src'),
+        extensions: ['.ts', '.js', '.json', '.css', '.html'],
         alias: {
-            "orion/editor/edit": "http://www.eclipse.org/orion/editor/releases/current/built-editor.min.js"
+            "orion/editor/edit": path.resolve("http://www.eclipse.org/orion/editor/releases/current/built-editor.min.js")
         }
     },
 
     module: {
-
-        preLoaders: [],
-
-        /*
-         * http://webpack.github.io/docs/configuration.html#module-loaders
-         * http://webpack.github.io/docs/list-of-loaders.html
-         */
-        loaders: [
+        rules: [
             {
                 test: /\.ts$/,
-                loaders: ['ts', 'angular2-template-loader'],
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
             },
-            {test: /\.html$/, loader: 'html'},
+            {
+                test: /\.html$/,
+                loader: 'html-loader'},
             {
                 test: /\.css$/,
                 exclude: helpers.root('src', 'app'),
-                loader: ExtractTextPlugin.extract(['css?sourceMap', 'postcss'])
+                loader: ExtractTextPlugin.extract(['css-loader?sourceMap'])
             },
             {
                 test: /\.css$/,
                 include: helpers.root('src', 'app'),
-                loaders: ['raw', 'postcss']
+                loaders: ['raw-loader']
             },
-            {
-                test: /\.scss$/,
-                exclude: helpers.root('src', 'app'),
-                loader: ExtractTextPlugin.extract(['css?sourceMap', 'postcss', 'resolve-url', 'sass'])
-            },
-            {
-                test: /\.scss$/,
-                include: helpers.root('src', 'app'),
-                loaders: ['raw', 'postcss', 'resolve-url', 'sass']
-            },
-            {test: /\.json$/, loader: 'json'},
             {
                 test: /\.(png|jpe?g|gif|ico|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url?limit=8192&mimetype=application/font-woff&name=assets/[name].[hash].[ext]'
+                loader: 'url-loader?limit=8192&mimetype=application/font-woff&name=assets/[name].[hash].[ext]'
             },
         ],
-
-        postLoaders: [],
     },
 
     /*
@@ -112,12 +75,22 @@ module.exports = {
      */
     plugins: [
 
+        new webpack.NamedModulesPlugin(),
         /*
          * https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
          */
-        new CommonsChunkPlugin({
+        new webpack.optimize.CommonsChunkPlugin({
             name: ['main', 'vendor', 'polyfills'],
         }),
+
+        /*
+         * https://github.com/angular/angular/issues/11580
+         */
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            helpers.root('src'),
+            {}
+        ),
 
         /*
          * https://www.npmjs.com/package/copy-webpack-plugin
@@ -134,25 +107,4 @@ module.exports = {
         }),
 
     ],
-
-    /*
-     * PostCSS
-     * https://github.com/postcss/postcss-loader
-     */
-    postcss: function () {
-        return [require('autoprefixer')];
-    },
-
-    /*
-     * Include polyfills or mocks for various node stuff
-     * https://webpack.github.io/docs/configuration.html#node
-     */
-    node: {
-        global: 'window',
-        crypto: 'empty',
-        process: false,
-        module: false,
-        clearImmediate: false,
-        setImmediate: false,
-    },
 };
