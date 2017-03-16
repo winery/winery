f4ee81ecb7b4328f994b74a0f790731fdb647d9c
Lukas Harzenetter <lharzenetter@gmx.de>
2017-03-16 17:06:07 +0100

    tslint 4
    Signed-off-by: Lukas Harzenetter <lharzenetter@gmx.de>

--- org.eclipse.winery.ui/src/app/section/section.component.ts
+++ org.eclipse.winery.ui/src/app/section/section.component.ts
@@ -9,18 +9,18 @@
  * Contributors:
  *     Lukas Harzenetter - initial API and implementation
  */
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { SectionService } from './section.service';
-import { SectionData } from './sectionData';
-import { NotificationService } from '../notificationModule/notificationservice';
-import { ValidatorObject } from '../validators/duplicateValidator.directive';
 import { isNullOrUndefined } from 'util';
 import { SectionResolverData } from '../interfaces/resolverData';
+import { NotificationService } from '../notificationModule/notificationservice';
+import { ValidatorObject } from '../validators/duplicateValidator.directive';
+import { SectionService } from './section.service';
+import { SectionData } from './sectionData';
 
-const showAll: string = 'Show all Items';
-const showGrouped: string = 'Group by Namespace';
+const showAll = 'Show all Items';
+const showGrouped = 'Group by Namespace';
 
 @Component({
     selector: 'winery-section-component',
@@ -34,12 +34,12 @@ const showGrouped: string = 'Group by Namespace';
 })
 export class SectionComponent implements OnInit, OnDestroy {
 
-    loading: boolean = true;
+    loading = true;
     selectedResource: string;
     routeSub: Subscription;
-    filterString: string = '';
-    itemsPerPage: number = 10;
-    showNamespace: string = 'all';
+    filterString = '';
+    itemsPerPage = 10;
+    showNamespace = 'all';
     changeViewButtonTitle: string = showGrouped;
     componentData: SectionData[];
 
@@ -47,7 +47,7 @@ export class SectionComponent implements OnInit, OnDestroy {
     newComponentNamespace: string;
     validatorObject: ValidatorObject;
 
-    fileOver: boolean = false;
+    fileOver = false;
 
     @ViewChild('addModal') addModal: any;
     @ViewChild('addComponentForm') addComponentForm: any;
@@ -121,7 +121,7 @@ export class SectionComponent implements OnInit, OnDestroy {
                     this.notify.error('Error while uploading CSAR file');
                 }
 
-                return { item, response, status, headers };
+                return {item, response, status, headers};
             };
         }
     }
