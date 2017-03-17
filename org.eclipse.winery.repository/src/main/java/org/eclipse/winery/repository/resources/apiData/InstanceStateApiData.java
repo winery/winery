bb963d4d8eaccf419ccb1dc291e1f2e3d38992fe
Philipp Meyer <meyer.github@gmail.com>
2017-03-17 12:07:12 +0100

    Fix checkstyle
    Signed-off-by: Philipp Meyer <meyer.github@gmail.com>

--- org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/resources/apiData/InstanceStateApiData.java
+++ org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/resources/apiData/InstanceStateApiData.java
@@ -3,7 +3,7 @@ package org.eclipse.winery.repository.resources.apiData;
 public class InstanceStateApiData {
     public String state;
 
-    public InstanceStateApiData(){
+    public InstanceStateApiData() {
 	}
 
 	public InstanceStateApiData(String state) {
