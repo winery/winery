05495bcca40dfec699c813efbc366cdd864d55c4
Oliver Kopp <kopp.dev@gmail.com>
2017-03-27 13:00:00 +0200

    Be aware of definitions wrapper
    Signed-off-by: Oliver Kopp <kopp.dev@gmail.com>

--- org.eclipse.winery.repository/src/test/java/org/eclipse/winery/repository/resources/entitytypes/nodetypes/NodeTypeResourceTest.java
+++ org.eclipse.winery.repository/src/test/java/org/eclipse/winery/repository/resources/entitytypes/nodetypes/NodeTypeResourceTest.java
@@ -32,7 +32,7 @@ public class NodeTypeResourceTest extends AbstractResourceTest {
 	@Test
 	public void baboabInitialExistsUsingRest() throws Exception {
 		this.setRevisionTo("5b5ad1106a3a428020b6bc5d2f154841acb5f779");
-		this.assertGet("nodetypes/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fnodetypes%252Ffruits/baobab/", "entitytypes/nodetypes/baobab_initial.xml");
+		this.assertGet("nodetypes/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fnodetypes%252Ffruits/baobab/", "entitytypes/nodetypes/baobab_initial_with_definitions.xml");
 	}
 
 	@Test
