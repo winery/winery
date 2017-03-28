c2228d575cad9e7ce82d2fc6be2221192c92b882
Oliver Kopp <kopp.dev@gmail.com>
2017-03-29 00:11:45 +0200

    Add test for empty list of node type implementations

--- org.eclipse.winery.repository/src/test/java/org/eclipse/winery/repository/resources/entitytypes/nodetypes/NodeTypeResourceTest.java
+++ org.eclipse.winery.repository/src/test/java/org/eclipse/winery/repository/resources/entitytypes/nodetypes/NodeTypeResourceTest.java
@@ -55,4 +55,10 @@ public class NodeTypeResourceTest extends AbstractResourceTest {
 		this.assertNotFound("nodetypes/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fnodetypes%252Ffruits/baobab/");
 	}
 
+	@Test
+	public void baboabHasNoImplementations() throws Exception {
+		this.setRevisionTo("5b5ad1106a3a428020b6bc5d2f154841acb5f779");
+		this.assertGetSize("nodetypes/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fnodetypes%252Ffruits/baobab/implementations", 0);
+	}
+
 }
