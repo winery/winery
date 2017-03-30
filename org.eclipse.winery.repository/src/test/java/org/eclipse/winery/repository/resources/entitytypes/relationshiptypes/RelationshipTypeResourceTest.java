e0274ec4b9c4924f2ca68acadbd88b51b978242a
Lukas Balzer <balzer814_dev@web.de>
2017-03-30 16:55:21 +0200

    created relationshipTypeTest
    Signed-off-by: Lukas Balzer <balzer814_dev@web.de>

---
+++ org.eclipse.winery.repository/src/test/java/org/eclipse/winery/repository/resources/entitytypes/relationshiptypes/RelationshipTypeResourceTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     Lukas Balzer - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.winery.repository.resources.entitytypes.relationshiptypes;
+
+import org.eclipse.winery.repository.resources.AbstractResourceTest;
+
+import org.junit.Test;
+
+public class RelationshipTypeResourceTest extends AbstractResourceTest {
+
+	@Test
+	public void createRelationshipType() throws Exception {
+		this.setRevisionTo("9c486269f6280e0eb14730d01554e7e4553a3d60");
+		this.assertPost("relationshiptypes/", "entitytypes/relationshiptypes/kiwi_create.json");
+		this.assertGet("relationshiptypes/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Frelationshiptypes%252Ffruits/kiwi/", "entitytypes/relationshiptypes/kiwi_initial.json");
+	}
+
+	@Test
+	public void kiwiVisualAppearance() throws Exception {
+		this.assertGet("relationshiptypes/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Frelationshiptypes%252Ffruits/kiwi/visualappearance/", "entitytypes/relationshiptypes/kiwi_initial.json");
+	}
+
+}
