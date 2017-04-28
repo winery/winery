ec1098d17addd97a9337b0637cdee5fa6e57aa84
Nicole Keppler <keppler1github@gmail.com>
2017-04-28 15:26:10 +0200

    initial API
    Signed-off-by: Nicole Keppler <keppler1github@gmail.com>

--- org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/resources/entitytypes/relationshiptypes/RelationshipTypeResource.java
+++ org.eclipse.winery.repository/src/main/java/org/eclipse/winery/repository/resources/entitytypes/relationshiptypes/RelationshipTypeResource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2013 University of Stuttgart.
+ * Copyright (c) 2012-2017 University of Stuttgart.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and the Apache License 2.0 which both accompany this distribution,
@@ -8,10 +8,9 @@
  *
  * Contributors:
  *     Oliver Kopp - initial API and implementation
+ *     Nicole Keppler - changes for angular2 validsourcesandtargets
  *******************************************************************************/
 package org.eclipse.winery.repository.resources.entitytypes.relationshiptypes;
-
-import java.util.Collection;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -22,7 +21,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.namespace.QName;
 
-import org.eclipse.winery.common.ids.definitions.NodeTypeId;
 import org.eclipse.winery.common.ids.definitions.RelationshipTypeId;
 import org.eclipse.winery.model.tosca.TExtensibleElements;
 import org.eclipse.winery.model.tosca.TRelationshipType;
@@ -32,12 +30,13 @@ import org.eclipse.winery.model.tosca.TRelationshipType.ValidSource;
 import org.eclipse.winery.model.tosca.TRelationshipType.ValidTarget;
 import org.eclipse.winery.model.tosca.TTopologyElementInstanceStates;
 import org.eclipse.winery.repository.backend.BackendUtils;
-import org.eclipse.winery.repository.backend.Repository;
+import org.eclipse.winery.repository.datatypes.select2.Select2DataItem;
+import org.eclipse.winery.repository.resources.apiData.ValidEndingsApiData;
+import org.eclipse.winery.repository.resources.apiData.ValidEndingsApiDataSet;
 import org.eclipse.winery.repository.resources.entitytypes.InstanceStatesResource;
 import org.eclipse.winery.repository.resources.entitytypes.TopologyGraphElementEntityTypeResource;
 import org.eclipse.winery.repository.resources.interfaces.InterfacesResource;
 
-import com.sun.jersey.api.view.Viewable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -78,7 +77,7 @@ public class RelationshipTypeResource extends TopologyGraphElementEntityTypeReso
 			interfaces = new SourceInterfaces();
 			this.getRelationshipType().setSourceInterfaces(interfaces);
 		}
-		return new InterfacesResource("source", interfaces.getInterface(), this);
+		return new InterfacesResource(this, interfaces.getInterface(), "source");
 	}
 
 	@Path("targetinterfaces/")
@@ -88,64 +87,64 @@ public class RelationshipTypeResource extends TopologyGraphElementEntityTypeReso
 			interfaces = new TargetInterfaces();
 			this.getRelationshipType().setTargetInterfaces(interfaces);
 		}
-		return new InterfacesResource("target", interfaces.getInterface(), this);
+		return new InterfacesResource(this, interfaces.getInterface(), "target");
 	}
 
-	@Path("validendings/")
+	/*
+	 * Select2DataItem used to send valid source as json to frontend.
+	 * Mapping from Select2DataItem.Id as ValidSource.namespace and
+	 * from Select2DataItem.Text as Validsource.localname
+	 */
+	@Path("validsourcesandtargets/")
 	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public Response getHTML() {
-		Viewable viewable = new Viewable("/jsp/entitytypes/relationshiptypes/validendings.jsp", this);
-		return Response.ok().entity(viewable).build();
-	}
-
-	@Path("validsource")
-	@GET
-	public String getValidSource() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public ValidEndingsApiData getValidEndings() {
+		ValidEndingsApiData validEndingsData = new ValidEndingsApiData();
+		//ToDo enable validSource as RequirementType (not-yet-implemented)
 		ValidSource validSource;
 		if (((validSource = this.getRelationshipType().getValidSource()) == null) || (validSource.getTypeRef() == null)) {
-			return null;
+			validEndingsData.validSource = null;
+		} else {
+			validSource = this.getRelationshipType().getValidSource();
+			validEndingsData.validSource = new ValidEndingsApiDataSet(
+					"nodeType",
+					new Select2DataItem(validSource.getTypeRef().toString(), validSource.getTypeRef().getLocalPart()));
 		}
-		return this.getRelationshipType().getValidSource().getTypeRef().toString();
-	}
-
-	@Path("validsource")
-	@PUT
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response setValidSource(String typeRef) {
-		ValidSource vs = new ValidSource();
-		QName qname = QName.valueOf(typeRef);
-		vs.setTypeRef(qname);
-		this.getRelationshipType().setValidSource(vs);
-		return BackendUtils.persist(this);
-	}
-
-	@Path("validtarget")
-	@GET
-	public String getValidTarget() {
+		// ToDo enable validTarget as CapabilityType (not-yet-implemented)
 		ValidTarget validTarget;
 		if (((validTarget = this.getRelationshipType().getValidTarget()) == null) || (validTarget.getTypeRef() == null)) {
-			return null;
+			validEndingsData.validTarget = null;
+		} else {
+			validTarget = this.getRelationshipType().getValidTarget();
+			validEndingsData.validTarget = new ValidEndingsApiDataSet(
+					"nodeType",
+					new Select2DataItem(validTarget.getTypeRef().toString(), validTarget.getTypeRef().getLocalPart()));
 		}
-		return this.getRelationshipType().getValidTarget().getTypeRef().toString();
+		return validEndingsData;
 	}
 
-	@Path("validtarget")
+	@Path("validsourcesandtargets/")
 	@PUT
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response setValidTarget(String typeRef) {
-		ValidTarget vt = new ValidTarget();
-		QName qname = QName.valueOf(typeRef);
-		vt.setTypeRef(qname);
-		this.getRelationshipType().setValidTarget(vt);
-		return BackendUtils.persist(this);
-	}
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response putValidEnding(ValidEndingsApiData validEndings) {
+		if (validEndings.validSource == null || validEndings.validSource.validDataSet == null || validEndings.validSource.validEndingsSelectionType == null) {
+			this.getRelationshipType().setValidSource(null);
+		} else {
+			ValidSource vs = new ValidSource();
+			QName qname = QName.valueOf(validEndings.validSource.validDataSet.getId());
+			vs.setTypeRef(qname);
+			this.getRelationshipType().setValidSource(vs);
+		}
+		if (validEndings.validTarget == null || validEndings.validTarget.validDataSet == null || validEndings.validTarget.validEndingsSelectionType == null) {
+			this.getRelationshipType().setValidTarget(null);
+		} else {
+			ValidTarget vt = new ValidTarget();
+			QName qnameVT = QName.valueOf(validEndings.validTarget.validDataSet.getId());
+			vt.setTypeRef(qnameVT);
+			this.getRelationshipType().setValidTarget(vt);
+		}
 
-	/**
-	 * Required for validendings.jsp
-	 */
-	public Collection<NodeTypeId> getPossibleValidEndings() {
-		return Repository.INSTANCE.getAllTOSCAComponentIds(NodeTypeId.class);
+		return BackendUtils.persist(this);
 	}
 
 	/**
