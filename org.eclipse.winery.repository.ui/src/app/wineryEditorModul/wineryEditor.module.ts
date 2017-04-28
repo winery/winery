1c8b84e55e747d7b58ba702289111f7ea5adc7fd
Philipp Meyer <meyer.github@gmail.com>
2017-04-28 15:37:18 +0200

    inital API
    Signed-off-by: Philipp Meyer <meyer.github@gmail.com>

---
+++ org.eclipse.winery.repository.ui/src/app/wineryEditorModul/wineryEditor.module.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     Philipp Meyer - initial API and implementation
+ */
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { WineryEditorComponent } from './wineryEditor.component';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    exports: [
+        WineryEditorComponent
+    ],
+    declarations: [WineryEditorComponent],
+    providers: [],
+})
+export class WineryEditorModule {
+}
