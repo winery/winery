fb0dad5a0a5974c9974bf9c4a73a21cf15524645
Latife Sen <latifesen91@gmail.com>
2017-07-25 18:46:51 +0200

    Fixed much Checkstyle for the Pull Request.  Changes to be committed: 	modified:   org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/BPMN4Tosca2BpelTest.java 	modified:   org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/BpelPlanArtefactWriterTest.java 	modified:   org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/JsonParserTest.java 	new file:   org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/package-info.java

--- org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/BpelPlanArtefactWriterTest.java
+++ org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/BpelPlanArtefactWriterTest.java
@@ -8,7 +8,7 @@
  *
  * Contributors:
  *     Sebastian Wagner - initial API and implementation
- *******************************************************************************/
+ *****************************************************************************/
 package org.eclipse.winery.bpmn2bpel;
 
 import java.net.MalformedURLException;
@@ -16,39 +16,54 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
-import org.eclipse.winery.bpmn2bpel.planwriter.BpelPlanArtefactWriter;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.eclipse.winery.bpmn2bpel.model.ManagementFlow;
 import org.eclipse.winery.bpmn2bpel.parser.Bpmn4JsonParser;
 import org.eclipse.winery.bpmn2bpel.parser.ParseException;
 
-
+/**
+ * Class BpelPlanArtefactWriterTest.
+ */
 public class BpelPlanArtefactWriterTest {
-	
-	@Before
-	public void setUp() throws Exception {
-	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    /**
+     * Setup Method - runs before tests.
+     * @throws Exception if Errors occur
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	/**
-	 * Test method for {@link BpelPlanArtefactWriter#writePlan(ManagementFlow, java.net.URL)}.
-	 */
-	@Test
-	public void testWritePlan() throws MalformedURLException, ParseException, URISyntaxException {
-		Bpmn4JsonParser parser = new Bpmn4JsonParser();
-		URI uri = Paths.get("src/test/resources/bpmn4tosca/bpmn4tosca.json").toUri();
-		//Path testBpmn4JsonFile = Paths.get("C:/temp/bpmn4tosca/bppmn4tosca.json");
-		ManagementFlow mngmtFlow = parser.parse(uri);
+    /**
+     * tearDown Method - runs after Tests.
+     * @throws Exception if Errors occur
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
 
-//		BpelPlanArtefactWriter writer = new BpelPlanArtefactWriter();
-//		writer.writePlan(mngmtFlow, null);
-	}
+    /**
+     * Test method for
+     * {@link BpelPlanArtefactWriter#writePlan(ManagementFlow, java.net.URL)}.
+     *      *
+     * @throws ParseException
+     *         If json contains errors.
+     * @throws MalformedURLException
+     *         If file paths are not defined correctly.
+     * @throws URISyntaxException
+     *         If the Syntax of the URI contains an Error.
+
+     */
+    @Test
+    public void testWritePlan() throws MalformedURLException,
+            ParseException, URISyntaxException {
+        Bpmn4JsonParser parser = new Bpmn4JsonParser();
+        URI uri = Paths.get("src/test/resources/bpmn4tosca/bpmn4tosca.json")
+                .toUri();
+        parser.parse(uri);
+
+    }
 
 }
---
+++ org.eclipse.winery.bpmn4tosca.converter.tobpel/src/test/java/org/eclipse/winery/bpmn2bpel/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Lati on 25.07.17.
+ */
+package org.eclipse.winery.bpmn2bpel;
