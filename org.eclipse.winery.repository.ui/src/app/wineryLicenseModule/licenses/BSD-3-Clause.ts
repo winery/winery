6b6fa1775a5edcc88730a8d6ca2af2fb623b6938
Philipp Meyer <meyer.github@gmail.com>
2017-09-29 19:21:36 +0200

    Add full license text for license component
    Signed-off-by: Philipp Meyer <meyer.github@gmail.com>

---
+++ org.eclipse.winery.repository.ui/src/app/wineryLicenseModule/licenses/BSD-3-Clause.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v20.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ */
+export const license = 'Copyright [YEAR] [COPYRIGHT HOLDER]\n' +
+    '\n' +
+    'Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n' +
+    '\n' +
+    '1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n' +
+    '\n' +
+    '2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the' +
+    ' following disclaimer in the documentation and/or other materials provided with the distribution.\n' +
+    '\n' +
+    '3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote' +
+    ' products derived from this software without specific prior written permission.\n' +
+    '\n' +
+    'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,' +
+    ' INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE ' +
+    'DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, ' +
+    'SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;' +
+    ' LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,' +
+    ' STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,' +
+    ' EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.';
---
+++ org.eclipse.winery.repository.ui/src/app/wineryLicenseModule/licenses/Unlicense.ts
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2017 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v20.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ */
+export const license = 'This is free and unencumbered software released into the public domain.\n' +
+    '\n' +
+    'Anyone is free to copy, modify, publish, use, compile, sell, or\n' +
+    'distribute this software, either in source code form or as a compiled\n' +
+    'binary, for any purpose, commercial or non-commercial, and by any\n' +
+    'means.\n' +
+    '\n' +
+    'In jurisdictions that recognize copyright laws, the author or authors\n' +
+    'of this software dedicate any and all copyright interest in the\n' +
+    'software to the public domain. We make this dedication for the benefit\n' +
+    'of the public at large and to the detriment of our heirs and\n' +
+    'successors. We intend this dedication to be an overt act of\n' +
+    'relinquishment in perpetuity of all present and future rights to this\n' +
+    'software under copyright law.\n' +
+    '\n' +
+    'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,\n' +
+    'EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n' +
+    'MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n' +
+    'IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR\n' +
+    'OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\n' +
+    'ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n' +
+    'OTHER DEALINGS IN THE SOFTWARE.\n' +
+    '\n' +
+    'For more information, please refer to <http://unlicense.org/>';
