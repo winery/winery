e1ba5d71497d7ead16b9d17cefaf8d00918ca013
Oliver Kopp <kopp.dev@gmail.com>
2017-11-02 10:51:59 +0100

    Update gitinspector installation to use npm

--- generate-stats.bat
+++ generate-stats.bat
@@ -1,26 +1,28 @@
 @echo off
-GOTO EndOfLicense
-/**
- * Copyright (c) 2017 University of Stuttgart.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * and the Apache License 2.0 which both accompany this distribution,
- * and are available at http://www.eclipse.org/legal/epl-v20.html
- * and http://www.apache.org/licenses/LICENSE-2.0
- */
-:EndOfLicense
-
+rem Copyright (c) 2017 Contributors to the Eclipse Foundation
+rem .
+rem See the NOTICE file(s) distributed with this work for additional
+rem information regarding copyright ownership.
+rem .
+rem This program and the accompanying materials are made available under the
+rem terms of the Eclipse Public License 2.0 which is available at
+rem http://www.eclipse.org/legal/epl-2.0, or the Apache Software License 2.0
+rem which is available at https://www.apache.org/licenses/LICENSE-2.0.
+rem .
+rem SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+rem .
 echo Statistics generator for Pratical Course Winery:  SS 2017
-echo ==================================================================
+echo ==========================================================
 echo .
 echo Python needs to be installed.
 echo This script needs to be run in the current git checkout of Winery -
 echo the checkout has to be complete, i.e., no checkout with --depth 10.
 echo .
-echo gitinspector in the version 0.4.4 has to reside in C:\git-repos\gitinspector\gitinspector\gitinspector
+echo Install gitinspector by using npm install -g gitinspector
 echo .
-echo You can clone get it from https://github.com/ejwa/gitinspector/archive/v0.4.4.zip
-echo and run the build command on the setup.py file.
+echo It might help to execute "git config diff.renameLimit 999999" on the
+echo command line
 echo .
 pause
-py C:\git-repos\gitinspector\gitinspector.py -f java,html,ts,css,json,xml,md --since=2017-07-30 --grading -x "author:^(?!((Lukas Harzenetter)|(Lukas Balzer)|(Niko Stadelmaier)|(Tino Stadelmaier)|(Philipp Meyer)))" --format=htmlembedded ./ > ./statistics/output.html
+mkdir statistics
+gitinspector -f java,html,ts,css,json,xml,md --since=2017-07-30 --grading -x "author:^(?!((Lukas Harzenetter)|(Lukas Balzer)|(Niko Stadelmaier)|(Tino Stadelmaier)|(Philipp Meyer)))" --format=htmlembedded ./ > ./statistics/output.html
