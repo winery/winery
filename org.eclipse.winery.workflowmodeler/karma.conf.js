176aa4c0e73ebd52c25e55c6d07c744d467c138a
Oliver Kopp <kopp.dev@gmail.com>
2018-02-05 15:16:09 +0100

    Updating some 2017 copyrights

--- org.eclipse.winery.workflowmodeler/karma.conf.js
+++ org.eclipse.winery.workflowmodeler/karma.conf.js
@@ -1,16 +1,18 @@
-/**
- * Copyright (c) 2017 ZTE Corporation.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * and the Apache License 2.0 which both accompany this distribution,
- * and are available at http://www.eclipse.org/legal/epl-v20.html
- * and http://www.apache.org/licenses/LICENSE-2.0
+/*******************************************************************************
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
  *
- * Contributors:
- *     ZTE - initial API and implementation and/or initial documentation
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
  *
  * This file has been generated using `ng new` and adapted to winery's needs.
- */
+ *******************************************************************************/
 module.exports = function (config) {
     config.set({
         basePath: '',
