790bc7eb8220a7d931cf3cb9687c26c24e41c321
Oliver Kopp <kopp.dev@gmail.com>
2018-07-03 14:54:59 +0200

    Clean up comments in TDeploymentArtifact

--- org.eclipse.winery.model.tosca/src/main/java/org/eclipse/winery/model/tosca/TDeploymentArtifact.java
+++ org.eclipse.winery.model.tosca/src/main/java/org/eclipse/winery/model/tosca/TDeploymentArtifact.java
@@ -14,35 +14,17 @@
 
 package org.eclipse.winery.model.tosca;
 
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
+import java.util.Objects;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.namespace.QName;
-import java.util.Objects;
 
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
-/**
- * <p>Java class for tDeploymentArtifact complex type.
- * <p>
- * <p>The following schema fragment specifies the expected content contained within this class.
- * <p>
- * <pre>
- * &lt;complexType name="tDeploymentArtifact">
- *   &lt;complexContent>
- *     &lt;extension base="{http://docs.oasis-open.org/tosca/ns/2011/12}tExtensibleElements">
- *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
- *       &lt;attribute name="artifactType" use="required" type="{http://www.w3.org/2001/XMLSchema}QName" />
- *       &lt;attribute name="artifactRef" type="{http://www.w3.org/2001/XMLSchema}QName" />
- *       &lt;anyAttribute processContents='lax' namespace='##other'/>
- *     &lt;/extension>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "tDeploymentArtifact")
 public class TDeploymentArtifact extends TExtensibleElements implements HasName {
@@ -79,62 +61,32 @@ public class TDeploymentArtifact extends TExtensibleElements implements HasName 
         return Objects.hash(name, artifactType, artifactRef);
     }
 
-    /**
-     * Gets the value of the name property.
-     *
-     * @return possible object is {@link String }
-     */
     @NonNull
     @Override
     public String getName() {
         return name;
     }
 
-    /**
-     * Sets the value of the name property.
-     *
-     * @param value allowed object is {@link String }
-     */
     @Override
-    public void setName(String value) {
+    public void setName(@NonNull String value) {
         this.name = value;
     }
 
-    /**
-     * Gets the value of the artifactType property.
-     *
-     * @return possible object is {@link QName }
-     */
     @NonNull
     public QName getArtifactType() {
         return artifactType;
     }
 
-    /**
-     * Sets the value of the artifactType property.
-     *
-     * @param value allowed object is {@link QName }
-     */
-    public void setArtifactType(QName value) {
+    public void setArtifactType(@NonNull QName value) {
         this.artifactType = value;
     }
 
-    /**
-     * Gets the value of the artifactRef property.
-     *
-     * @return possible object is {@link QName }
-     */
     @Nullable
     public QName getArtifactRef() {
         return artifactRef;
     }
 
-    /**
-     * Sets the value of the artifactRef property.
-     *
-     * @param value allowed object is {@link QName }
-     */
-    public void setArtifactRef(QName value) {
+    public void setArtifactRef(@Nullable QName value) {
         this.artifactRef = value;
     }
 
