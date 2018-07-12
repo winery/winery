0896fb81cbe295f2bb25350b4c30f9e2f9594b9e
Christoph Krieger <krieger.christoph1@gmail.com>
2018-07-12 13:20:15 +0200

    add additional inital PlanType
    Signed-off-by: Christoph Krieger <krieger.christoph1@gmail.com>

--- org.eclipse.winery.repository.rest/src/main/java/org/eclipse/winery/repository/rest/resources/admin/types/PlanTypesManager.java
+++ org.eclipse.winery.repository.rest/src/main/java/org/eclipse/winery/repository/rest/resources/admin/types/PlanTypesManager.java
@@ -25,6 +25,7 @@ public class PlanTypesManager extends AbstractTypesManager {
         // add data without rendering in the plan types file
         this.addData(org.eclipse.winery.repository.Constants.TOSCA_PLANTYPE_BUILD_PLAN, "Build Plan");
         this.addData(org.eclipse.winery.repository.Constants.TOSCA_PLANTYPE_TERMINATION_PLAN, "Termination Plan");
+        this.addData(org.eclipse.winery.repository.Constants.TOSCA_PLANTYPE_MANAGEMENT_PLAN, "Management Plan");
     }
 
 }
