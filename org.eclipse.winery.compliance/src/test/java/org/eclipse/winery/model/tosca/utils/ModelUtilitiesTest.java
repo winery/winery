ab96bd6ba5408b39b25d6935f5ff79e57a37d9f9
Lukas Harzenetter <lharzenetter@gmx.de>
2018-09-04 09:47:16 +0200

    fix compilation errors

--- org.eclipse.winery.compliance/src/test/java/org/eclipse/winery/model/tosca/utils/ModelUtilitiesTest.java
+++ org.eclipse.winery.compliance/src/test/java/org/eclipse/winery/model/tosca/utils/ModelUtilitiesTest.java
@@ -17,11 +17,11 @@ package org.eclipse.winery.model.tosca.utils;
 import org.eclipse.winery.model.tosca.TNodeTemplate;
 import org.eclipse.winery.repository.TestWithGitBackedRepository;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ModelUtilitiesTest extends TestWithGitBackedRepository {
 
