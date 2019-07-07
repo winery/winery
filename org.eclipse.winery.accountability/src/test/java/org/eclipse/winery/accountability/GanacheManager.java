67f3220e7eed15b04fa0802985c5eb4cf0c2ef22
Tobias Piontek <tobias_piontek@gmx.de>
2019-07-07 23:54:52 +0200

    Signed-off-by: Tobias Piontek <tobias_piontek@gmx.de>
    Changes in Parameter Order

--- org.eclipse.winery.accountability/src/test/java/org/eclipse/winery/accountability/GanacheManager.java
+++ org.eclipse.winery.accountability/src/test/java/org/eclipse/winery/accountability/GanacheManager.java
@@ -27,7 +27,7 @@ public class GanacheManager {
         }
         
         else{
-            processBuilder.command("bash", "-c", "ganache-cli", "-l 0xfffffffffff", "-v", "--account=\"0xb1211de9fb36bc81c9e269e1f6b783d80e01f3709562bb78451d1a956b3c2038,99999999999999999999999999999999\"");
+            processBuilder.command("bash", "-c", "ganache-cli", "--account=\"0xb1211de9fb36bc81c9e269e1f6b783d80e01f3709562bb78451d1a956b3c2038,99999999999999999999999999999999\"" , "-l 0xfffffffffff", "-v");
 
         }
         this.process = processBuilder.start();
