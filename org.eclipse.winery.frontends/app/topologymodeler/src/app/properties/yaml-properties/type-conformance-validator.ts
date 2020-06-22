9d8661210766f591215127e95fe7277d6a718651
Clemens Lieb <vogel612@gmx.de>
2020-06-22 16:59:21 +0200

    Make the form display validation errors from type checks

---
+++ org.eclipse.winery.frontends/app/topologymodeler/src/app/properties/yaml-properties/type-conformance-validator.ts
@@ -0,0 +1,213 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *******************************************************************************/
+import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { TDataType } from '../../models/ttopology-template';
+import { QName } from '../../models/qname';
+// TODO this should possibly not be from tosca-management
+import { YamlPropertyDefinition } from '../../../../../tosca-management/src/app/model/yaml';
+import { Constraint, isWellKnown } from '../../../../../tosca-management/src/app/model/constraint';
+import { ConstraintChecking } from '../property-constraints';
+import { InheritanceUtils } from '../../models/InheritanceUtils';
+import { ToscaUtils } from '../../models/toscaUtils';
+
+export class TypeConformanceValidator implements Validator {
+
+    private fullTypeDefinition: string | { constraints: Constraint[] } | YamlPropertyDefinition[];
+    private laxParsing: boolean;
+
+    constructor(private dataTypes: TDataType[], private enforcedType: string | QName) {
+        this.precacheMetaInformation();
+    }
+
+    validate(control: AbstractControl): ValidationErrors | null {
+        // reset stored errors
+        const structuredValue = this.parseValue(control.value);
+        if (structuredValue === undefined) {
+            // this only happens if parsing is not lax OR the value could not be parsed as string after enquoting it
+            return { 'typeConformance':  [ 'Could not parse entered value as JSON' ]};
+        }
+        // we cannot perform "static typechecking" on property functions defined in Section 4.4 of the spec
+        if (isPropertyFunction(structuredValue)) {
+            return null;
+        }
+        return this.fulfilsTypeDefinition(structuredValue);
+    }
+
+    private precacheMetaInformation() {
+        if (isWellKnown(this.enforcedType)) {
+            this.fullTypeDefinition = this.enforcedType;
+            // these known types need to be parseable as objects because they are
+            this.laxParsing = this.enforcedType !== 'list' && this.enforcedType !== 'map' && this.enforcedType !== 'range';
+            return;
+        }
+
+        const typeIdentifier: string = (this.enforcedType instanceof QName) ? this.enforcedType.qName : this.enforcedType;
+        const dataTypeInheritance = InheritanceUtils.getInheritanceAncestry(typeIdentifier, this.dataTypes);
+        if (dataTypeInheritance.some(t => t.properties || ToscaUtils.getDefinition(t).properties)) {
+            // assume that there's no constraints here, since the constraints don't really work on complex types in the first place
+            this.fullTypeDefinition = this.handleComplexDataType(dataTypeInheritance);
+            this.laxParsing = false;
+            return;
+        }
+        // FIXME deal with checking for the basetype of the constrained type?!
+        // aggregate constraints through the hierarchy
+        const allConstraints = [];
+        for (const ancestor of dataTypeInheritance) {
+            // no need to check for ancestor properties, that's handled by handleComplexDataType
+            for (const c of ToscaUtils.getDefinition(ancestor).constraints) {
+                allConstraints.push(c);
+            }
+        }
+        this.fullTypeDefinition = { constraints: allConstraints };
+        this.laxParsing = true;
+    }
+
+    private handleComplexDataType(hierarchy: TDataType[]): YamlPropertyDefinition[] {
+        const result = [];
+        // it's useful to assume that the types themselves in the hierarchy do not have constraints
+        // as such we only need to aggregate the properties enforced by each of the
+        for (const parent of hierarchy) {
+            for (const property of parent.properties) {
+                // FIXME if necessary create a type definition for these ones as well!
+                result.push(property);
+            }
+        }
+        return result;
+    }
+
+    private fulfilsTypeDefinition(structuredValue: any): ValidationErrors | null {
+        if (!this.fullTypeDefinition) {
+            console.warn('No full type-definition was computed for type ' + this.enforcedType);
+            return null;
+        }
+        if (typeof this.fullTypeDefinition === 'string') {
+            return this.fulfilsWellKnownType(structuredValue, this.fullTypeDefinition) ? null
+                : { 'typeConformance': [ `Value was not conform to TOSCA-YAML well known type ${this.fullTypeDefinition}.` ]};
+        }
+        if (!this.fullTypeDefinition['constraints'] !== undefined) {
+            // @ts-ignore Typescript doesn't correctly narrow the union type here
+            return this.fulfilsKnownConstraints(structuredValue, this.fullTypeDefinition);
+        }
+        // @ts-ignore Typescript doesn't correctly narrow the union type here
+        return this.fulfilsPropertyRequirements(structuredValue, this.fullTypeDefinition);
+    }
+
+    private fulfilsWellKnownType(structuredValue: any, knownType: string): boolean {
+        switch (knownType) {
+            case 'string':
+                // consider that this might need to also accept stuff that's parseable as number, boolean or anything else
+                return typeof structuredValue === 'string';
+            case 'integer':
+            case 'float':
+                return typeof structuredValue === 'number';
+            case 'boolean':
+                return typeof structuredValue === 'boolean' || structuredValue === 'yes' || structuredValue === 'no';
+            case 'timestamp':
+                // FIXME actual check for conformance to typestamp value
+                return typeof structuredValue === 'string';
+            case 'null':
+                // why ever you'd want to do this?
+                return structuredValue === null || structuredValue === undefined;
+            case 'version':
+                return typeof structuredValue === 'string' && structuredValue.match(/\d+\.\d+(\.\d+(\..+?(-\d+)?)?)?/) !== undefined;
+            case 'range':
+                return structuredValue.isArray && structuredValue.length === 2;
+            case 'list':
+                return structuredValue.isArray;
+            case 'map':
+                return structuredValue.isObject;
+            case 'scalar-unit.size':
+            case 'scalar-unit.time':
+            case 'scalar-unit.frequency':
+            case 'scalar-unit.bitrate':
+                // FIXME check unit conformance
+                return typeof structuredValue === 'string';
+            default:
+                // not actually one of the well-known types here!
+                return false;
+        }
+    }
+
+    private fulfilsKnownConstraints(structuredValue: any, requirements: { constraints: Constraint[] }): ValidationErrors | null {
+        let valid = true;
+        const errors: ValidationErrors = [];
+        for (const constraint of requirements.constraints) {
+            if (!ConstraintChecking.isValid({ operator: constraint.key, value: constraint.list || constraint.value }, structuredValue)) {
+                errors.push(`Value does not conform to constraint "${constraint.key} - ${constraint.list || constraint.value}"`);
+                valid = false;
+            }
+        }
+        return valid ? null : { 'typeConformance': errors };
+    }
+
+    private fulfilsPropertyRequirements(structuredValue: any, properties: YamlPropertyDefinition[]): ValidationErrors | null {
+        let valid = true;
+        const errors: ValidationErrors = [];
+        if (structuredValue === undefined || structuredValue === null) {
+            // FIXME deal with the question whether the form field is required
+            return properties.some(prop => prop.required)
+                ? [ `${this.enforcedType} has defined required properties!` ]
+                : null;
+        }
+        for (const member in structuredValue) {
+            if (properties.find(prop => prop.name === member) !== undefined) {
+                errors.push(`Includes the member ${member} that is not defined on the type`);
+                valid = false;
+            }
+        }
+        for (const requiredProperty of properties.filter(prop => prop.required)) {
+            if (structuredValue[requiredProperty.name] === undefined) {
+                errors.push(`Does not include the required member ${requiredProperty.name}`);
+                valid = false;
+            }
+        }
+        return valid ? null : {'typeConformance' : errors };
+    }
+
+    private parseValue(value: string): any {
+        let result;
+        try {
+            result = JSON.parse(value);
+        } catch (e) {
+            if (!this.laxParsing) {
+                // the value is not actually a string or something deriving from it
+                // therefore we expect something that's parseable as JSON and bail here
+                return undefined;
+            }
+            // try reparsing as string
+            try {
+                // this should never ever fail because we should be able to parse literally anything as a string, so long as we enquote it
+                result = JSON.parse( '"' + value + '"');
+            } catch (e) {
+                return undefined;
+            }
+        }
+        return result;
+    }
+}
+
+const function_keys: string[] = [
+    'get_input', 'get_property', 'get_attribute', 'get_operation_output', 'get_nodes_of_type', 'get_artifact'
+];
+
+function isPropertyFunction(structuredValue: any) {
+    if (structuredValue === undefined || structuredValue === null) { return false; }
+    for (const func of function_keys) {
+        if (structuredValue.hasOwnProperty(func)) {
+            // TODO evaluate whether we'd need to check for that key to be the only key present?
+            return true;
+        }
+    }
+    return false;
+}
--- org.eclipse.winery.frontends/app/topologymodeler/src/app/properties/yaml-properties/typeaware-input.component.ts
+++ org.eclipse.winery.frontends/app/topologymodeler/src/app/properties/yaml-properties/typeaware-input.component.ts
@@ -13,18 +13,13 @@
  *******************************************************************************/
 
 import { Component, forwardRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AbstractControl, ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
 import { TDataType } from '../../models/ttopology-template';
 import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { BackendService } from '../../services/backend.service';
-// FIXME these probably shouldn't be the management constraints
-import { Constraint, isWellKnown } from '../../../../../tosca-management/src/app/model/constraint';
 import { QName } from '../../models/qname';
-import { InheritanceUtils } from '../../models/InheritanceUtils';
-import { ToscaUtils } from '../../models/toscaUtils';
-import { YamlPropertyDefinition } from '../../../../../tosca-management/src/app/model/yaml';
-import { ConstraintChecking } from '../property-constraints';
+import { TypeConformanceValidator } from './type-conformance-validator';
 
 /**
  * This is an input component that is aware of the DataType that the value it receives must conform to.
@@ -42,14 +37,14 @@ import { ConstraintChecking } from '../property-constraints';
             useExisting: forwardRef(() => TypeawareInputComponent),
             multi: true
         },
-        // {
-        //     provide: NG_VALIDATORS,
-        //     useExisting: forwardRef(() => TypeawareInputComponent),
-        //     multi: true
-        // },
+        {
+            provide: NG_VALIDATORS,
+            useExisting: forwardRef(() => TypeawareInputComponent),
+            multi: true
+        },
     ]
 })
-export class TypeawareInputComponent implements ControlValueAccessor, OnInit, OnChanges {
+export class TypeawareInputComponent implements ControlValueAccessor, OnInit, OnChanges, Validator {
 
     @Input()
     type: QName | string;
@@ -62,13 +57,12 @@ export class TypeawareInputComponent implements ControlValueAccessor, OnInit, On
     // storage for callbacks
     private _onChange: any = e => {};
     private _onTouch: any = e => {};
+    private desugaredValidator: Validator = undefined;
 
     // this subject helps reduce computation load by debouncing validation triggers while the user adds input.
     private validationDebouncer: Subject<any> = new Subject<any>();
     private availableDataTypes: TDataType[] = [];
 
-    private fullTypeDefinition: string | { constraints: Constraint[] } | YamlPropertyDefinition[];
-    private laxParsing: boolean;
     JSON: JSON;
 
     constructor(private dataTypes: BackendService) {
@@ -80,7 +74,7 @@ export class TypeawareInputComponent implements ControlValueAccessor, OnInit, On
     ngOnInit() {
         this.validationDebouncer.pipe(
             debounceTime(350), distinctUntilChanged(),
-        ).subscribe(value => this.validateAndDispatch(value));
+        ).subscribe(value => this._onChange(value));
         // this is a fix to make the global javascript object available inside the component template
         this.JSON = JSON;
     }
@@ -89,7 +83,7 @@ export class TypeawareInputComponent implements ControlValueAccessor, OnInit, On
         if (changes.type) {
             // compute the complete type definition including all required properties for this value
             this.type = changes.type.currentValue;
-            this.computeApplicablePropertyDefinitions();
+            this.desugaredValidator = new TypeConformanceValidator(this.availableDataTypes, this.type);
         }
     }
 
@@ -98,46 +92,8 @@ export class TypeawareInputComponent implements ControlValueAccessor, OnInit, On
         this._onTouch(target);
     }
 
-    private validateAndDispatch(value: string): void {
-        // reset stored errors
-        this.errors = [];
-        const structuredValue = this.parseValue(value);
-        if (structuredValue === undefined) {
-            // this only happens if parsing is not lax or the value could not be parsed as string after enquoting it
-            this.pushError('Could not parse entered value as JSON');
-            return;
-        }
-        // we cannot perform "static typechecking" on property functions defined in Section 4.4 of the spec
-        if (isPropertyFunction(structuredValue)) {
-            this._onChange(structuredValue);
-            return;
-        }
-        // TODO perform actual static typechecking here
-        if (this.fulfilsTypeDefinition(structuredValue)) {
-            this._onChange(structuredValue);
-            return;
-        }
-    }
-
-    private parseValue(value: string): any {
-        let result;
-        try {
-            result = JSON.parse(value);
-        } catch (e) {
-            if (!this.laxParsing) {
-                // the value is not actually a string or something deriving from it
-                // therefore we expect something that's parseable as JSON and bail here
-                return undefined;
-            }
-            // try reparsing as string
-            try {
-                // this should never ever fail because we should be able to parse literally anything as a string, so long as we enquote it
-                result = JSON.parse( '"' + value + '"');
-            } catch (e) {
-                return;
-            }
-        }
-        return result;
+    validate(control: FormControl): ValidationErrors | null {
+        return this.desugaredValidator !== undefined && this.desugaredValidator.validate(control);
     }
 
     registerOnChange(fn: any): void {
@@ -165,144 +121,4 @@ export class TypeawareInputComponent implements ControlValueAccessor, OnInit, On
         }
         return value;
     }
-
-    private fulfilsTypeDefinition(structuredValue: any) {
-        if (!this.fullTypeDefinition) {
-            console.warn('No full type-definition was computed for type ' + this.type);
-            return true;
-        }
-        if (typeof this.fullTypeDefinition === 'string') {
-            return this.fulfilsWellKnownType(structuredValue, this.fullTypeDefinition);
-        }
-        if (!this.fullTypeDefinition['constraints'] !== undefined) {
-            // @ts-ignore Typescript doesn't correctly narrow the union type here
-            return this.fulfilsKnownConstraints(structuredValue, this.fullTypeDefinition);
-        }
-        // @ts-ignore Typescript doesn't correctly narrow the union type here
-        return this.fulfilsPropertyRequirements(structuredValue, this.fullTypeDefinition);
-    }
-
-    private computeApplicablePropertyDefinitions() {
-        if (isWellKnown(this.type)) {
-            this.fullTypeDefinition = this.type;
-            // these known types need to be parseable as objects because they are
-            this.laxParsing = this.type !== 'list' && this.type !== 'map' && this.type !== 'range';
-            return;
-        }
-
-        const typeIdentifier: string = (this.type instanceof QName) ? this.type.qName : this.type;
-        const dataTypeInheritance = InheritanceUtils.getInheritanceAncestry(typeIdentifier, this.availableDataTypes);
-        if (dataTypeInheritance.some(t => t.properties || ToscaUtils.getDefinition(t).properties)) {
-            // assume that there's no constraints here, since the constraints don't really work on complex types in the first place
-            this.fullTypeDefinition = this.handleComplexDataType(dataTypeInheritance);
-            this.laxParsing = false;
-            return;
-        }
-        // FIXME deal with checking for the basetype of the constrained type?!
-        // aggregate constraints through the hierarchy
-        const allConstraints = [];
-        for (const ancestor of dataTypeInheritance) {
-            // no need to check for ancestor properties, that's handled by handleComplexDataType
-            for (const c of ToscaUtils.getDefinition(ancestor).constraints) {
-                allConstraints.push(c);
-            }
-        }
-        this.fullTypeDefinition = { constraints: allConstraints };
-        this.laxParsing = true;
-    }
-
-    private handleComplexDataType(hierarchy: TDataType[]): YamlPropertyDefinition[] {
-        const result = [];
-        // it's useful to assume that the types themselves in the hierarchy do not have constraints
-        // as such we only need to aggregate the properties enforced by each of the
-        for (const parent of hierarchy) {
-            for (const property of parent.properties) {
-                // FIXME if necessary create a type definition for these ones as well!
-                result.push(property);
-            }
-        }
-        return result;
-    }
-
-    private pushError(message: string) {
-        this.errors.push(message);
-    }
-
-    private fulfilsWellKnownType(structuredValue: any, knownType: string) {
-        switch (knownType) {
-            case 'string':
-                return typeof structuredValue === 'string';
-            case 'integer':
-            case 'float':
-                return typeof structuredValue === 'number';
-            case 'boolean':
-                return typeof structuredValue === 'boolean' || structuredValue === 'yes' || structuredValue === 'no';
-            case 'timestamp':
-                // FIXME actual check for conformance to typestamp value
-                return typeof structuredValue === 'string';
-            case 'null':
-                // why ever you'd want to do this?
-                return structuredValue === null || structuredValue === undefined;
-            case 'version':
-                return typeof structuredValue === 'string' && structuredValue.match(/\d+\.\d+(\.\d+(\..+?(-\d+)?)?)?/);
-            case 'range':
-                return structuredValue.isArray && structuredValue.length === 2;
-            case 'list':
-                return structuredValue.isArray;
-            case 'map':
-                return structuredValue.isObject;
-            case 'scalar-unit.size':
-            case 'scalar-unit.time':
-            case 'scalar-unit.frequency':
-            case 'scalar-unit.bitrate':
-                // FIXME check unit conformance
-                return typeof structuredValue === 'string';
-            default:
-                // not actually one of the well-known types here!
-                return false;
-        }
-    }
-
-    private fulfilsKnownConstraints(structuredValue: any, requirements: { constraints: Constraint[] }) {
-        let valid = true;
-        for (const constraint of requirements.constraints) {
-            if (!ConstraintChecking.isValid({ operator: constraint.key, value: constraint.list || constraint.value }, structuredValue)) {
-                this.pushError(`Value does not conform to constraint "${constraint.key} - ${constraint.list || constraint.value}"`);
-                valid = false;
-            }
-        }
-        return valid;
-    }
-
-    private fulfilsPropertyRequirements(structuredValue: any, properties: YamlPropertyDefinition[]) {
-        let valid = true;
-        for (const member in structuredValue) {
-            if (properties.find(prop => prop.name === member) !== undefined) {
-                this.pushError(`Includes the member ${member} that is not defined on the type`);
-                valid = false;
-            }
-        }
-        for (const requiredProperty of properties.filter(prop => prop.required)) {
-            if (structuredValue[requiredProperty.name] === undefined) {
-                this.pushError(`Does not include the required member ${requiredProperty.name}`);
-                valid = false;
-            }
-        }
-        return valid;
-    }
-}
-
-
-const function_keys: string[] = [
-    'get_input', 'get_property', 'get_attribute', 'get_operation_output', 'get_nodes_of_type', 'get_artifact'
-];
-
-function isPropertyFunction(structuredValue: any) {
-    for (const func of function_keys) {
-        if (structuredValue.hasOwnProperty(func)) {
-            // TODO evaluate whether we'd need to check for that key to be the only key present?
-            return true;
-        }
-    }
-    return false;
 }
