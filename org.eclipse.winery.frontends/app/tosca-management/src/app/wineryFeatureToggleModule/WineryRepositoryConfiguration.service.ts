53cab4bed2b15096e15e041b8f894941e7ae73a2
Lukas Harzenetter <lharzenetter@gmx.de>
2022-06-14 11:11:23 +0200

    Adds Feature to Use the Deployment Normalization Assistant (DNA) Service (#677)

--- org.eclipse.winery.frontends/app/tosca-management/src/app/wineryFeatureToggleModule/WineryRepositoryConfiguration.service.ts
+++ org.eclipse.winery.frontends/app/tosca-management/src/app/wineryFeatureToggleModule/WineryRepositoryConfiguration.service.ts
@@ -39,6 +39,7 @@ export interface WineryConfiguration {
         propertyCheck: boolean;
         yaml: boolean;
         patternDetection: boolean;
+        normalization: boolean;
     };
     endpoints: {
         bpmnModeler: string;
@@ -48,6 +49,7 @@ export interface WineryConfiguration {
         edmmTransformationTool: string;
         eclipseChe: string;
         tops: string;
+        deploymentNormalizationAssistant: string;
     };
     git: {
         clientId: string;
@@ -63,6 +65,7 @@ export interface WineryRepositoryConfiguration {
 
 @Injectable()
 export class WineryRepositoryConfigurationService {
+
     configuration: WineryConfiguration;
 
     constructor(private http: HttpClient) {
