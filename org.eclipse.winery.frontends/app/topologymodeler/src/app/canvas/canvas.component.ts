312f90b7a0533196486ef6474d14c30fc14707d7
Karoline Saatkamp <karoline.saatkamp@iaas.uni-stuttgart.de>
2022-07-27 08:22:21 +0200

    fix proptery handling in topology modeler

--- org.eclipse.winery.frontends/app/topologymodeler/src/app/canvas/canvas.component.ts
+++ org.eclipse.winery.frontends/app/topologymodeler/src/app/canvas/canvas.component.ts
@@ -1542,7 +1542,6 @@ export class CanvasComponent implements OnInit, OnDestroy, OnChanges, AfterViewI
                 this.ngRedux.dispatch(this.actions.sendPaletteOpened(false));
             }
         }
-        this.ngRedux.dispatch(this.topologyRendererActions.highlightNodes(this.selectedNodes.map(node => node.id)));
     }
 
     /**
